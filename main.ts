1d25860047c06e97c0ea53c1dc493a17a54c6282
zyrkio <61494336+zyrkio@users.noreply.github.com>
2023-12-17 12:48:58 +0100

    case with string

--- main.ts
+++ main.ts
@@ -1,64 +1,54 @@
 //% color="#AA278D" icon="\uf2fe" block="Threshold Calculator"
 namespace ThresholdCalculator {
     let values: number[] = [];
-
+    let thresholdMw = 0;
 
     /**
-     * Calculate the threshold 
-     * @returns The calculated threshold value.
+     * Berechne den Threshold
+     * @param signal Beschreibe den Parameter hier
+     * @param caseDescription Beschreibe den Fall hier ("reset" löscht und setzt auf 0)
      */
-    //% block="calculate threshold $signal"
-    //% block="Konvertiere, filtere, gleiche aus und berechne RMS des EMG-Signal $signal"
-    //% signal.min=0 signal.max=1023
-    export function calculateThreshold(num: number): number {
-        values.push(num);
-        
-        if (values.length === 0) {
-            return 0; // Wenn keine Werte vorhanden sind, gib 0 als Standard-Schwelle zurück
-        }
-        if (num === 10) {
-            // Wenn der Wert 10 ist, ignorieren und direkt 0 zurückgeben
-            values = [];
+    //% block="Berechne den Mittelwert von $signal für den Fall $caseDescription"
+    //% signal.min=0 signal.max=5
+    export function calculateThreshold(signal: number, caseDescription: string): number {
 
-            // Fülle das Array mit Nullen entsprechend der gewünschten Länge
-            
-            for (let i = 0; i < values.length; i++) {
-                values.push(0);
-            }
+        if (caseDescription.toLowerCase() === "reset") {
+            values = [];
             return 0;
         }
 
-       
+        values.push(signal);
 
-       
+        if (values.length === 0) {
+            return 0; // If no values provided, return 0 as default threshold
+        }
+
         let sum = 0;
         let maxValues: number[] = [];
         let minValues: number[] = [];
 
         for (let i = 0; i < values.length; i++) {
-            if (values[i] !== 10) {
-                sum += values[i];
+            sum += values[i];
 
-                if (maxValues.length === 0 || values[i] > maxValues[0]) {
-                    maxValues = [values[i]];
-                } else if (values[i] === maxValues[0]) {
-                    maxValues.push(values[i]);
-                }
+            if (maxValues.length === 0 || values[i] > maxValues[0]) {
+                maxValues = [values[i]];
+            } else if (values[i] === maxValues[0]) {
+                maxValues.push(values[i]);
+            }
 
-                if (minValues.length === 0 || values[i] < minValues[0]) {
-                    minValues = [values[i]];
-                } else if (values[i] === minValues[0]) {
-                    minValues.push(values[i]);
-                }
+            if (minValues.length === 0 || values[i] < minValues[0]) {
+                minValues = [values[i]];
+            } else if (values[i] === minValues[0]) {
+                minValues.push(values[i]);
             }
         }
 
-        const filteredLength = values.filter(val => val !== 10).length;
-        const average = filteredLength > 0 ? sum / filteredLength : 0;
+        const average = sum / values.length;
         const maxAverage = calculateAverage(maxValues);
         const minAverage = calculateAverage(minValues);
 
-        return (maxAverage + minAverage) / 2;
+        thresholdMw = (maxAverage + minAverage) / 2;
+        return thresholdMw;
     }
 
     function calculateAverage(numbers: number[]): number {
