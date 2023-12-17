54d1bd35fd38089b56375b5b2183f9247544cf89
zyrkio <61494336+zyrkio@users.noreply.github.com>
2023-12-17 12:13:05 +0100

    reset function

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
+
 //% color="#AA278D" icon="\uf2fe" block="Threshold Calculator"
 namespace ThresholdCalculator {
     let values: number[] = [];
-    let thresholdMw = 0;
+
 
     /**
-     * Berechne den Threshold
-     * @param signal, beschreibe den Parameter hier
+     * Calculate the threshold 
+     * @returns The calculated threshold value.
      */
-    //% block="Berechne den Mittelwert $signal"
-    //% signal.min=0 signal.max=5
-    export function calculateThreshold(signal : number): number {
+    //% block="calculate threshold $signal"
+    //% block="Konvertiere, filtere, gleiche aus und berechne RMS des EMG-Signal $signal"
+    //% signal.min=0 signal.max=1023
+    export function calculateThreshold(num: number): number {
+        if (num === 10) {
+            // Wenn der Wert 10 ist, setze das Array auf 0 zurück
+            values = [];
+            return 0; // Gib 0 zurück
+        }
 
-        values.push(signal );
+        values.push(num);
 
         if (values.length === 0) {
             return 0; // If no values provided, return 0 as default threshold
@@ -41,8 +48,7 @@ namespace ThresholdCalculator {
         const maxAverage = calculateAverage(maxValues);
         const minAverage = calculateAverage(minValues);
 
-        thresholdMw = (maxAverage + minAverage) / 2;
-        return thresholdMw;
+        return (maxAverage + minAverage) / 2;
     }
 
     function calculateAverage(numbers: number[]): number {
